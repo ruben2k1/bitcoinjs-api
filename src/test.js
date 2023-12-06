709d2ffa90c3739eb3ed050982776d6cd307c049
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-06 03:48:48 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,8 +1,9 @@
-const bitcoin = require('./lib/Bitcoin');
-const Bitcoin = new bitcoin();
+const Bitcoin = require('./lib/Bitcoin');
 
 (async () => {
-    const address = await Bitcoin.getUtxos('bc1qput0gcms80khke8w6wgj433yglm9x8djg8u6w7', 'MAINNET');
+    const bitcoin = new Bitcoin();
+    
+    const utxos = await bitcoin.getUtxos('bc1qput0gcms80khke8w6wgj433yglm9x8djg8u6w7', 'MAINNET');
 
-    console.log(address);
+    console.log(utxos);
 })();
