384e7b785fd7f1565764d6c6a502f73a38cbef68
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-06 03:23:28 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,12 +1,8 @@
 const bitcoin = require('./lib/Bitcoin');
-const btc = require('bitcoinjs-lib');
 const Bitcoin = new bitcoin();
 
-const address = Bitcoin.generateSegwitP2WSHAddress3of4(
-'026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01',
-'02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9',
-'023e4740d0ba639e28963f3476157b7cf2fb7c6fdf4254f97099cf8670b505ea59',
-'03c6103b3b83e4a24a0e33a4df246ef11772f9992663db0c35759a5e2ebf68d8e9'
-);
+(async () => {
+    const address = await Bitcoin.getUtxos('bc1qput0gcms80khke8w6wgj433yglm9x8djg8u6w7', 'MAINNET');
 
-console.log(address);
+    console.log(address);
+})();
