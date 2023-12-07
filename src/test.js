875084ba3f79ddfe9c30f2008c84af411dbd4b9b
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-07 02:59:05 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,9 +1,10 @@
 const Bitcoin = require('./lib/Bitcoin');
+const btc = require('bitcoinjs-lib');
 
 (async () => {
     const bitcoin = new Bitcoin();
     
-    const utxos = await bitcoin.getUtxos('bc1qput0gcms80khke8w6wgj433yglm9x8djg8u6w7', 'MAINNET');
+    const WIF = bitcoin.getWIFfromXPRIV('xprv9s21ZrQH143K4DRBUU8Dp25M61mtjm9T3LsdLLFCXL2U6AiKEqs7dtCJWGFcDJ9DtHpdwwmoqLgzPrW7unpwUyL49FZvut9xUzpNB6wbEnz', btc.networks.bitcoin);
 
-    console.log(utxos);
+    console.log(WIF);
 })();
