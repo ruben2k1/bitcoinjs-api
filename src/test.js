f4d362e1fbfde940adcc152febc514481be701d2
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-07 18:27:03 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const Bitcoin = require('./lib/Bitcoin');
 (async () => {
     const bitcoin = new Bitcoin();
 
-    const results = await bitcoin.getUtxos('bc1qput0gcms80khke8w6wgj433yglm9x8djg8u6w7', 'MAINNET'); //MAINNET OR TESTNET
+    const results = await bitcoin.getEstimatedFees(bitcoin.networks.bitcoin);
 
     console.log(results);
 })();
