ef3772afba39447e38626a00c90ee6cc7bfa7ff7
Rubén <56169753+ruben2k1@users.noreply.github.com>
2023-12-07 04:04:47 +0100

    Update test.js

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ const Bitcoin = require('./lib/Bitcoin');
 (async () => {
     const bitcoin = new Bitcoin();
     
-    const WIF = bitcoin.getWIFfromXPRIV('xprv9s21ZrQH143K4DRBUU8Dp25M61mtjm9T3LsdLLFCXL2U6AiKEqs7dtCJWGFcDJ9DtHpdwwmoqLgzPrW7unpwUyL49FZvut9xUzpNB6wbEnz', bitcoin.networks.bitcoin);
+    const results = bitcoin.generateMnemonicAndNativeSegwit(bitcoin.networks.bitcoin);
 
-    console.log(WIF);
+    console.log(results);
 })();
